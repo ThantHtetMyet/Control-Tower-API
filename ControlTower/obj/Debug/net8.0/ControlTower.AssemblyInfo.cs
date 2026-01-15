3867f3fe8275059bf91c49702ce72906e69586fe
Thant Htet Myet <thanthtetmyet1994@gmail.com>
2026-01-15 10:35:33 +0800

    Update_SQLScript

--- ControlTower/obj/Debug/net8.0/ControlTower.AssemblyInfo.cs
+++ ControlTower/obj/Debug/net8.0/ControlTower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlTower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169bd1a305dd6142f7a099f06eba067cb5b3bec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b19eb4bfd653cf002a5870787899bd81b00d50")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlTower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlTower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
